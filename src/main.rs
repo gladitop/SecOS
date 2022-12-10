abcdde3acd72c9ce94e7d7e5367e039ba3c86d73
CryptoGladi <mymamchm@mail.ru>
2022-12-10 21:16:29 +0300

    Работает?

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
 
+pub mod gdt;
 pub mod gpu;
+pub mod interrupts;
 
-use gpu::vga::VGA;
 use core::panic::PanicInfo;
 
 /// This function is called on kernel panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("Kernel panic!");
+    println!("Info error: {}", info);
+
     loop {}
+}
+
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut vga = VGA::init();
-    vga.println(b"Welcom to SecOS \n WARNING: THIS IS ONLY A PROTOTYPE");
-    //vga.println(b"WWWW");
-
-
-    loop {
-        
+    {
+        let mut writer = gpu::vga::WRITER.lock();
+        writer.set_background();
     }
+
+    println!("Loading kernel service...");
+    init();
+
+    println!("Kernel done!");
+    println!("Welcome to OC");
+    println!("It's a prototype!");
+    println!("Version: 0.1");
+    println!("Author: Almaev Damir Maratovich");
+
+    loop {}
 }
